5da2d78b58d0dda09ab31c56f25266467cedd85d
Andreas B. Berg <berg.andreasb@gmail.com>
2024-02-28 17:43:41 +0100

    Add hello world

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+import os
+from slack_bolt import App
+from slack_sdk import WebClient
+
+client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+def send_message(message, channel_id = "C06MNH9JYJC"):
+    # Call the conversations.list method using the WebClient
+    result = client.chat_postMessage(
+        channel=channel_id,
+        text=message
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    send_message("Hello from Python!")
