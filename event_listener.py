32586bd3b58be6ff0ab8e8aa3b42cee369ef7a89
Andreas B. Berg <berg.andreasb@gmail.com>
2024-02-28 19:06:50 +0100

    Add response-to-messages template

--- event_listener.py
+++ event_listener.py
@@ -1,7 +1,8 @@
 import os
 from slack_bolt import App
 from websockets.sync.client import connect
-import time
+from hello_world import send_message
+import json
 
 app = App()
 
@@ -9,19 +10,32 @@ def open_socket():
     token = os.environ.get("SLACK_APP_LEVEL_TOKEN")
     return app.client.apps_connections_open(app_token=token)
 
-def listen_for_events(url, event_type, callback):
+def listen_for_events(url, should_reply):
     with connect(url) as websocket:
         while(True):
-            message = websocket.recv()
-            print(message)
+            message = json.loads(websocket.recv())
+            print("MESSAGE: ", message)
+            try:
+                event = message["payload"]["event"]
+                if should_reply(event["text"]):
+                    channel_id = event["channel"]
+                    ts = event["ts"]
+                    send_message("Pong (take that!)", channel_id, ts)
+            except Exception as e:
+                print(e)
         
 
+def listen_for_text(wanted_text):
+    def listener(message):
+        if wanted_text in message:
+            return True
+    return listener
 
 if __name__ == "__main__":
     result = open_socket()
     print(result)
     
     if result.get("ok"):
-        listen_for_events(result.get("url"), "", "")
+        listen_for_events(result.get("url"), listen_for_text("Ping"))
 
     
--- hello_world.py
+++ hello_world.py
@@ -4,10 +4,11 @@ from slack_sdk import WebClient
 
 client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 
-def send_message(message, channel_id = "C06MNH9JYJC"):
+def send_message(message, channel_id = "C06MNH9JYJC", thread_ts = None):
     # Call the conversations.list method using the WebClient
     result = client.chat_postMessage(
         channel=channel_id,
+        thread_ts=thread_ts,
         text=message
     )
     print(result)
