0031345478c79308a2e7060546d26cacc1ba1798
Andreas B. Berg <berg.andreasb@gmail.com>
2024-05-15 18:56:55 +0200

    Siste faggruppemøte: sBert

--- sBert.py
+++ sBert.py
@@ -7,9 +7,11 @@ from hello_world import send_message
 import json
 import pandas as pd
 from pathlib import Path
-import spacy
 
-nlp = spacy.load("nb_core_news_lg")
+from sentence_transformers import SentenceTransformer, util
+
+model = SentenceTransformer('models/221')
+
 
 def open_socket():
     app = App()
@@ -32,37 +34,34 @@ def listen_for_events(url, get_reply):
                     send_message(get_reply(event["text"]), channel_id, ts)
             except Exception as e:
                 print("ERROR: ", e)
-        
+
 
 def listen_for_text(wanted_text):
     def listener(message):
         if wanted_text in message:
             return True
+
     return listener
 
 
 def get_data(data_path: Path = Path().absolute() / "data" / "processed" / "all_top_2023.txt"):
     data_ = pd.read_csv(data_path, delimiter=",", quotechar="|", dtype="str").dropna(axis=0)
-    data_["doc"] = [nlp(message) for message in data_['message']]
+    data_["embedding"] = [model.encode(message) for message in data_['message']]
     return data_
 
 
 if __name__ == "__main__":
     data = get_data()
-    print(data.iloc[0]["message"], data.iloc[0]["doc"].vector)
-    print(data.iloc[0]["doc"].similarity(data.iloc[1]["doc"]))
 
     def get_reply_fn(new_message):
-        data["similarity"] = data["doc"].apply(lambda doc: doc.similarity(nlp(new_message)))
+        encoded = model.encode(new_message)
+        data["similarity"] = data["embedding"].apply(lambda emb: util.cos_sim(emb, encoded).item())
         reply_message = data.nlargest(1, 'similarity')
-        return (f"This looks like the message:"
-                f"\n>{reply_message["message"].item()}"
-                f"\n(ts: {reply_message["ts"].item()}, similarity: {reply_message["similarity"].item()})")
+        return (f"This looks like the message: \n> {reply_message["message"].item()}\n"
+                f"(ts: {reply_message["ts"].item()}, similarity: {reply_message["similarity"].item()})")
 
     result = open_socket()
     print(result)
 
     if result.get("ok"):
         listen_for_events(result.get("url"), get_reply_fn)
-
-    
